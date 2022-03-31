9c345ac845573e6aa64788537cec5bdcf5f4fa36
juanma rivero <45921725+riverojm@users.noreply.github.com>
2022-03-31 15:44:31 -0300

    eliminacion de 8 y nueve, crear 12

---
+++ Assets/SCRIPTS/Ejercicio12.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio12 : MonoBehaviour
+{
+    // En un casino de juegos se desea mostrar los mensajes respectivos por el puntaje  obtenido en el lanzamiento de tres dados de un cliente, de acuerdo a los siguientes  resultados:  
+    //• Si los tres dados son seis, mostrar el mensaje “Excelente” 
+    //• Si dos dados son seis, mostrar el mensaje “Muy bien”  
+    //• Si un dado es seis, mostrar el mensaje “Regular”  
+    //• Si ningún dado se obtiene seis, mostrar el mensaje “Insuficiente”
+
+    // Start is called before the first frame update
+    public int n1;
+    public int n2;
+    public int n3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
